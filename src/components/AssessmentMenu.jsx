664b4b7a7669dc502c90b2bf9b0b78e2568a35f3
mrx7355608 <mrx7355608@gmail.com>
2024-07-20 04:46:15 +0500

    deleting assessments

--- src/components/AssessmentMenu.jsx
+++ src/components/AssessmentMenu.jsx
@@ -1,9 +1,14 @@
 import { Menu, MenuButton, MenuItem, MenuList, Button } from "@chakra-ui/react";
+import { useState } from "react";
 import { HiDotsVertical } from "react-icons/hi";
 import { Link } from "react-router-dom";
+import useToastUtils from "../hooks/useToastUtils";
 
 // eslint-disable-next-line
 export default function AssessmentMenu({ id }) {
+    const { showErrorToast, showSuccessToast } = useToastUtils();
+    const [loading, setLoading] = useState(false);
+
     return (
         <Menu>
             <MenuButton
@@ -22,8 +27,34 @@ export default function AssessmentMenu({ id }) {
                 </Link>
                 <MenuItem>Make private</MenuItem>
                 <MenuItem>Change password</MenuItem>
-                <MenuItem textColor="red.400">Delete</MenuItem>
+                <MenuItem textColor="red.400" onClick={deleteAssessment}>
+                    {loading ? "Deleting..." : "Delete"}
+                </MenuItem>
             </MenuList>
         </Menu>
     );
+
+    async function deleteAssessment() {
+        try {
+            setLoading(true);
+            const options = {
+                method: "delete",
+                credentials: "include",
+            };
+            const response = await fetch(
+                `${import.meta.env.VITE_SERVER_URL}/api/assessments/${id}`,
+                options,
+            );
+            if (!response.ok) {
+                const result = await response.json();
+                return showErrorToast(result.error);
+            }
+
+            showSuccessToast("Assessment has been deleted");
+        } catch (err) {
+            showErrorToast("An un-expected error occurred");
+        } finally {
+            setLoading(false);
+        }
+    }
 }
