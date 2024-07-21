900e6c9a2a2f0b981a590f4f5288c6bbadc1d43a
mrx7355608 <mrx7355608@gmail.com>
2024-07-22 04:32:09 +0500

    using react-doc-viewer to display doc files

--- src/components/DocxViewer.jsx
+++ src/components/DocxViewer.jsx
@@ -1,30 +1,18 @@
-import { useEffect, useState } from "react";
-import mammoth from "mammoth";
+import { Box } from "@chakra-ui/react";
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
+import "@cyntler/react-doc-viewer/dist/index.css";
 
 export default function DocxViewer({ id }) {
-    const [content, setContent] = useState("");
-
-    useEffect(() => {
-        const fetchAndConvertDocx = async () => {
-            try {
-                const response = await fetch(
-                    `${import.meta.env.VITE_SERVER_URL}/api/assessments/view-assessment-file/${id}`,
-                );
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-
-                const arrayBuffer = await response.arrayBuffer();
-                const result = await mammoth.convertToHtml({ arrayBuffer });
-
-                setContent(result.value);
-            } catch (error) {
-                console.error("Error fetching or converting document:", error);
-            }
-        };
-
-        fetchAndConvertDocx();
-    }, [id]);
-
-    return <div dangerouslySetInnerHTML={{ __html: content }}></div>;
+    return (
+        <Box height={"100vh"} my={5}>
+            <DocViewer
+                documents={[
+                    {
+                        uri: `${import.meta.env.VITE_SERVER_URL}/api/assessments/view-assessment-file/${id}`,
+                    },
+                ]}
+                pluginRenderers={DocViewerRenderers}
+            />
+        </Box>
+    );
 }
