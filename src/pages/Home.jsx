29819909758eb84ca62750548aa2bd86bbb42384
mrx7355608 <mrx7355608@gmail.com>
2024-09-18 22:03:44 +0500

    implemented infinite scrolling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,31 @@
-import { Flex, Text, Heading, Divider } from "@chakra-ui/react";
+import { Flex, Text, Heading, Divider, Spinner } from "@chakra-ui/react";
 import FilterModal from "../components/files/FilterModal";
 import SortMenu from "../components/SortMenu";
 import HomeSearchbar from "../components/search/HomeSearchbar";
-import AssessmentsContainer from "../components/files/AssessmentsContainer";
 import { useSearchParams } from "react-router-dom";
 import ErrorBoundary from "../components/main/ErrorBoundary";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useState, useEffect } from "react";
+import useFetch from "../hooks/useFetch";
+import AssessmentItem from "../components/files/AssessmentItem";
 
 export default function Home() {
     // eslint-disable-next-line
     const [sp, _setSp] = useSearchParams();
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
+    const [files, setFiles] = useState([]);
+    const { data } = useFetch(`/api/files?${sp.toString()}`);
+
+    useEffect(() => {
+        if (!data) return;
+
+        if (data.length < 10) {
+            setHasMore(false);
+        }
+
+        setFiles([...files, ...data]);
+    }, [data]);
 
     return (
         <>
@@ -25,9 +42,22 @@ export default function Home() {
             <ErrorBoundary
                 fallback={<Text color={"red.500"}>Something went wrong!</Text>}
             >
-                <AssessmentsContainer
-                    endpoint={`/api/files?${sp.toString()}`}
-                />
+                <InfiniteScroll
+                    dataLength={files.length}
+                    next={() => {
+                        setPage(page + 1);
+                        sp.set("page", page);
+                    }}
+                    hasMore={hasMore}
+                    loader={<Spinner marginX={"auto"} />}
+                    scrollThreshold={0.95}
+                >
+                    <Flex flexWrap={"wrap"} gap={4} mb={20}>
+                        {files.map((a) => {
+                            return <AssessmentItem key={a.id} data={a} />;
+                        })}
+                    </Flex>
+                </InfiniteScroll>
             </ErrorBoundary>
         </>
     );
