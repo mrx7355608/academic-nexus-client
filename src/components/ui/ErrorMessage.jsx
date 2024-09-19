b919d30bac15b016e61e0fdbd3442772538ee542
mrx7355608 <mrx7355608@gmail.com>
2024-09-19 13:01:24 +0500

    added Error Alert in ErrorMessage component

--- src/components/ui/ErrorMessage.jsx
+++ src/components/ui/ErrorMessage.jsx
@@ -1,5 +1,10 @@
-import { Text } from "@chakra-ui/react";
+import { Alert, AlertIcon } from "@chakra-ui/react";
 
 export default function ErrorMessage({ err }) {
-    return <Text color="red.500">{err}</Text>;
+    return (
+        <Alert status="error">
+            <AlertIcon />
+            {err}
+        </Alert>
+    );
 }
