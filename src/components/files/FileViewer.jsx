573ee1980fa4392292e25d5921cce0336632e107
mrx7355608 <mrx7355608@gmail.com>
2024-09-19 13:01:02 +0500

    pre-fetching file to display error in-case of invalid url or any other error

--- src/components/files/FileViewer.jsx
+++ src/components/files/FileViewer.jsx
@@ -1,19 +1,38 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Box, Spinner } from "@chakra-ui/react";
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 import "@cyntler/react-doc-viewer/dist/index.css";
+import ErrorMessage from "../ui/ErrorMessage";
 
 const FileViewer = React.memo(({ fileURL }) => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        fetch(fileURL)
+            .then((resp) => {
+                if (!resp.ok) {
+                    setError("Unable to load file");
+                }
+            })
+            .catch(() => setError("Unable to load file"))
+            .finally(setLoading(false));
+    }, [fileURL]);
+
     return (
         <Box height={"100vh"} my={5}>
-            <DocViewer
-                documents={[
-                    {
-                        uri: fileURL,
-                    },
-                ]}
-                pluginRenderers={DocViewerRenderers}
-            />
+            {loading && <Spinner />}
+            {error && <ErrorMessage err={error} />}
+            {!error && !loading ? (
+                <DocViewer
+                    documents={[
+                        {
+                            uri: fileURL,
+                        },
+                    ]}
+                    pluginRenderers={DocViewerRenderers}
+                />
+            ) : null}
         </Box>
     );
 });
